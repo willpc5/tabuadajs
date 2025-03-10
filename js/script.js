ffa4e7aa3f2d5797cfbca08a87cd3d01b4e0635b
Willams Silva <willamssilvainfo@gmail.com>
2025-03-09 22:18:39 -0300

    Iniciando com o JavaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("Teste");
+// Seleção de elementos
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+
+
+// Funções
+
+// Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    const multiplicationNumber = +numberInput.value;
+  
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if (!multiplicationNumber || !multiplicatorNumber ) return;
+    
+  
+    console.log(multiplicationNumber, multiplicatorNumber);
+  });
