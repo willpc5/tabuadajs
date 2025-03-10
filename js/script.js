1c80114b2b37938c6b455b7b4a6cc96784087a05
Willams Silva <willamssilvainfo@gmail.com>
2025-03-10 13:51:47 -0300

    Finalizando o projeto

--- js/script.js
+++ js/script.js
@@ -3,8 +3,36 @@ const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
 
+const multiplicationTitle = document.querySelector(
+  "#multiplication-title span"
+);
+
+const multiplicationTable = document.querySelector(
+  "#multiplication-operations"
+);
 
 // Funções
+const createTable = (number, multiplicatorNumber) => {
+  multiplicationTable.innerHTML = "";
+
+  for (i = 1; i <= multiplicatorNumber; i++) {
+    const result = number * i;
+
+    const template = `<div class="row">
+            <div class="operation">${number} x ${i} = </div>
+            <div class="result">${result}</div>
+          </div>`;
+
+    const parser = new DOMParser();
+    const htmlTemplate = parser.parseFromString(template, "text/html");
+    const row = htmlTemplate.querySelector(".row");
+
+    multiplicationTable.appendChild(row);
+
+  }
+
+  multiplicationTitle.innerText = number;
+};
 
 // Eventos
 multiplicationForm.addEventListener("submit", (e) => {
@@ -15,7 +43,7 @@ multiplicationForm.addEventListener("submit", (e) => {
     const multiplicatorNumber = +multiplicationInput.value;
 
     if (!multiplicationNumber || !multiplicatorNumber ) return;
+
+    createTable(multiplicationNumber, multiplicatorNumber);
     
-  
-    console.log(multiplicationNumber, multiplicatorNumber);
   });
